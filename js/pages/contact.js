47f4ec51ec668aab7dd931250e259c77d205f1d5
yakitori-server-admin <civilization.destruction@gmail.com>
2023-05-04 17:40:36 +0900

    Add a file via upload.

--- js/pages/contact.js
+++ js/pages/contact.js
@@ -1,10 +1,10 @@
 form.addEventListener("submit", function(event) {
-    var form = document.getElementById("form"); // #formというidを持つ<form>要素を取得
-    var inputs = form.querySelectorAll("input, textarea"); // <form>要素内のすべての<input>と<textarea>要素を取得
-    for (var i = 0; i < inputs.length; i++) { // 繰り返し処理で
-      inputs[i].readOnly = true; // 各入力要素にreadonly属性をtrueに設定
+    let form = document.getElementById("form");
+    let inputs = form.querySelectorAll("input, textarea");
+    for (let i = 0; i < inputs.length; i++) {
+      inputs[i].readOnly = true;
     }
-    var button = document.querySelector(".button[type='submit']"); // .button[type='submit']というセレクターに一致する要素を取得
-    button.classList.add("disabled"); // .disabledクラスを付与
-    button.textContent = "送信済み"; // テキストを"送信済み"に変更
+    let button = document.querySelector(".button[type='submit']");
+    button.classList.add("disabled");
+    button.textContent = "送信済み";
 });
